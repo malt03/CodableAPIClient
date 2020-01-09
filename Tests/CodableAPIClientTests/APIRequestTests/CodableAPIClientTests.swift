f60137b8977034e4b7005f5bc44f12656311eeb0
Koji Murata <koji.murata@dena.com>
2020-01-09 16:17:01 +0900

    test

--- Tests/CodableAPIClientTests/APIRequestTests/CodableAPIClientTests.swift
+++ Tests/CodableAPIClientTests/APIRequestTests/CodableAPIClientTests.swift
@@ -2,46 +2,31 @@ import XCTest
 @testable import CodableAPIClient
 
 final class CodableAPIClientTests: XCTestCase {
-    let successTestCases: [Runnable] = [
+    let testCases: [Runnable] = [
         APIRequestTestCase(SuccessRequest(method: .delete), "http://localhost/delete",        1, .init(args: .nil(), json: .init()), nil),
         APIRequestTestCase(SuccessRequest(method: .get),    "http://localhost/get?a=c&b=d", nil, .init(args: .init(), json: nil),    nil),
         APIRequestTestCase(SuccessRequest(method: .patch),  "http://localhost/patch",         1, .init(args: .nil(), json: .init()), nil),
         APIRequestTestCase(SuccessRequest(method: .post),   "http://localhost/post",          1, .init(args: .nil(), json: .init()), nil),
         APIRequestTestCase(SuccessRequest(method: .put),    "http://localhost/put",           1, .init(args: .nil(), json: .init()), nil),
-    ]
     
-    let statusCodeTestCases: [Runnable] = [
+        // Status Code
         APIRequestTestCase(StatusCodeRequest(statusCode: 301), "http://localhost/status/301", nil, .init(), nil),
         APIRequestTestCase(StatusCodeRequest(statusCode: 400), "http://localhost/status/400", nil, nil,     "400 Bad Request"),
         APIRequestTestCase(StatusCodeRequest(statusCode: 500), "http://localhost/status/500", nil, nil,     "500 Internal Server Error"),
-    ]
-    
-    let unexpectedMimeTypeTestCases: [Runnable] = [
+        
+        // Unexpected Mime Type
         APIRequestTestCase(XMLRequest(), "http://localhost/xml", nil, nil, "Unexpected Mime Type: application/xml"),
-    ]
-    
-    let delayTestCases: [Runnable] = [
+
+        // Delay
         APIRequestTestCase(DelayRequest(delay: 2, timeoutInterval: 1), "http://localhost/delay/2", nil, nil,    "The request timed out."),
         APIRequestTestCase(DelayRequest(delay: 2, timeoutInterval: 3), "http://localhost/delay/2", nil, .init(), nil),
     ]
 
-    func testSuccess() {
-        successTestCases.forEach { $0.run() }
-    }
-
-    func testStatusCode() {
-        statusCodeTestCases.forEach { $0.run() }
-    }
-    
-    func testUnexpectedMimeType() {
-        unexpectedMimeTypeTestCases.forEach { $0.run() }
-    }
-
-    func testDelay() {
-        delayTestCases.forEach { $0.run() }
+    func test() {
+        testCases.forEach { $0.run() }
     }
 
     static var allTests = [
-        ("testSuccess", testSuccess),
+        ("test", test),
     ]
 }
